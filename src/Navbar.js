e26bbcb9f77c3135e39138090af8ea19a1c45758
kukurudz-nadia <kukurudznadia6@gmail.com>
2024-07-15 11:31:03 +0300

    add navbar and homepage

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The dance blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/create">Create a new blog</a>
+      </div>
+    </nav>
+  );
+
+}
+
+export default NavBar;
