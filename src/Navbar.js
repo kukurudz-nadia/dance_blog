5ba79694a271f30716c08b841a879244ed84ae27
kukurudz-nadia <kukurudznadia6@gmail.com>
2024-07-16 08:46:31 +0300

    add blogs to homepage

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
       <h1>The dance blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">Create a new blog</a>
+        <a href="/create" style={{color: 'white', backgroundColor: 'rgb(89, 128, 232)', borderRadius: '8px'}}>Create a blog</a>
       </div>
     </nav>
   );
