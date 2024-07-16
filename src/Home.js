2a8432e437938abca52b7f885f0fdc0bb061e1c6
kukurudz-nadia <kukurudznadia6@gmail.com>
2024-07-16 08:46:31 +0300

    add blogs to homepage

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,34 @@
-import { useState } from 'react';
+import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    { title: 'New dance tango!', body: 'This is a description about tango', author: 'selena', id: 1 },
-    { title: 'Vacancy for a dance teacher!', body: 'This is a description about vacancy for a dance teacher', author: 'bill', id: 2 },
-    { title: "Mia won 'The last dance competition' in New York", body: 'This is a description about winner', author: 'selena', id: 3 },
+    {
+      title: "New dance tango!",
+      body: "This is a description about tango",
+      author: "selena",
+      id: 1,
+    },
+    {
+      title: "Vacancy for a dance teacher!",
+      body: "This is a description about vacancy for a dance teacher",
+      author: "bill",
+      id: 2,
+    },
+    {
+      title: "Mia won 'The last dance competition' in New York",
+      body: "This is a description about winner",
+      author: "selena",
+      id: 3,
+    },
   ]);
 
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <div>{ blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
-        </div>
-      )) }</div>
+      <BlogList blogs={blogs} title={ 'All blogs!' } />
     </div>
   );
-}
+};
 
 export default Home;
