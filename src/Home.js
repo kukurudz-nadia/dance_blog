bf5617eee8a8712e931335a3b5d9842f2de1a903
kukurudz-nadia <kukurudznadia6@gmail.com>
2024-07-18 10:37:22 +0300

    cratead blogList component

--- src/Home.js
+++ src/Home.js
@@ -23,10 +23,19 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
-      <BlogList blogs={blogs} title={ 'All blogs!' } />
+      <BlogList
+        blogs={blogs}
+        title={"All blogs!"}
+        handleDelete={handleDelete}
+      />
     </div>
   );
 };
